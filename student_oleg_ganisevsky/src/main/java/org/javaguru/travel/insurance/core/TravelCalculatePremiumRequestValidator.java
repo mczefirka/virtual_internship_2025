4280b1aaf59f224f2774767cc11c50b84de5f55e
Oleg Ganisevsky <ganisevskij@gmail.com>
2025-03-09 12:34:32 +0300

    STEP 030 DONE

--- student_oleg_ganisevsky/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidator.java
+++ student_oleg_ganisevsky/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidator.java
@@ -14,12 +14,19 @@ class TravelCalculatePremiumRequestValidator {
     public List<ValidationError> validate(TravelCalculatePremiumRequest request) {
         List<ValidationError> errors = new ArrayList<>();
         validatePersonFirstName(request).ifPresent(errors::add);
+        validatePersonLastName(request).ifPresent(errors::add);
         return errors;
     }
 
     private Optional<ValidationError> validatePersonFirstName(TravelCalculatePremiumRequest request) {
         return (request.getPersonFirstName() == null || request.getPersonFirstName().isEmpty())
                 ? Optional.of(new ValidationError("personFirstName", "Must not be empty!"))
+                : Optional.empty();
+    }
+
+    private Optional<ValidationError> validatePersonLastName(TravelCalculatePremiumRequest request) {
+        return (request.getPersonLastName() == null || request.getPersonLastName().isEmpty())
+                ? Optional.of(new ValidationError("personLastName", "Must not be empty!"))
                 : Optional.empty();
     }
 
--- student_oleg_ganisevsky/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
+++ student_oleg_ganisevsky/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 public class TravelCalculatePremiumRequestValidatorTest {
     @Test
-    public void givenRequest_whenValidatePersonFirstName_thenDoNotReturnErrors() {
+    public void givenRequest_whenValidateRequest_thenDoNotReturnErrors() {
         // Populate request fields
         var request = new TravelCalculatePremiumRequest();
         request.setPersonFirstName("Sigma");
@@ -22,12 +22,12 @@ public class TravelCalculatePremiumRequestValidatorTest {
 
         var validator = new TravelCalculatePremiumRequestValidator();
 
-        // Check whether the validator created an error list. If the list isn't empty, the test passes
+        // Check whether the validator created an error list. If the list is empty, the test passes
         assertTrue(validator.validate(request).isEmpty());
     }
 
     @Test
-    public void givenRequestWithoutFirstName_whenValidatePersonFirstName_thenReturnErrors() {
+    public void givenRequestWithoutFirstName_whenValidateRequest_thenReturnErrors() {
         // Populate request fields
         var request = new TravelCalculatePremiumRequest();
 
@@ -44,13 +44,64 @@ public class TravelCalculatePremiumRequestValidatorTest {
     }
 
     @Test
-    public void givenRequestWithEmptyFirstName_whenValidatePersonFirstName_thenReturnErrors() {
+    public void givenRequestWithEmptyFirstName_whenValidateRequest_thenReturnErrors() {
         // Populate request fields
         var request = new TravelCalculatePremiumRequest();
 
         // Request with empty personFirstName field
         request.setPersonFirstName("");
         request.setPersonLastName("Male");
+        request.setAgreementDateFrom(new Date(new Date().getTime() - 2 * 86_400_000L));
+        request.setAgreementDateTo(new Date());
+
+        var validator = new TravelCalculatePremiumRequestValidator();
+
+        // Check whether the validator created an error list. If the list isn't empty, the test passes
+        assertFalse(validator.validate(request).isEmpty());
+    }
+
+    @Test
+    public void givenRequestWithoutLastName_whenValidateRequest_thenReturnErrors() {
+        // Populate request fields
+        var request = new TravelCalculatePremiumRequest();
+
+        // Request without personLastName field
+        request.setPersonFirstName("Sigma");
+//        request.setPersonLastName("Male");
+        request.setAgreementDateFrom(new Date(new Date().getTime() - 2 * 86_400_000L));
+        request.setAgreementDateTo(new Date());
+
+        var validator = new TravelCalculatePremiumRequestValidator();
+
+        // Check whether the validator created an error list. If the list isn't empty, the test passes
+        assertFalse(validator.validate(request).isEmpty());
+    }
+
+    @Test
+    public void givenRequestWithEmptyLastName_whenValidateRequest_thenReturnErrors() {
+        // Populate request fields
+        var request = new TravelCalculatePremiumRequest();
+
+        // Request with empty personLastName field
+        request.setPersonFirstName("Sigma");
+        request.setPersonLastName("");
+        request.setAgreementDateFrom(new Date(new Date().getTime() - 2 * 86_400_000L));
+        request.setAgreementDateTo(new Date());
+
+        var validator = new TravelCalculatePremiumRequestValidator();
+
+        // Check whether the validator created an error list. If the list isn't empty, the test passes
+        assertFalse(validator.validate(request).isEmpty());
+    }
+
+    @Test
+    public void givenRequestWithoutFirstNameAndLastName_whenValidateRequest_thenReturnErrors() {
+        // Populate request fields
+        var request = new TravelCalculatePremiumRequest();
+
+        // Request without personFirstName and personLastName fields
+/*      request.setPersonFirstName("Sigma");
+        request.setPersonLastName("Male");*/
         request.setAgreementDateFrom(new Date(new Date().getTime() - 2 * 86_400_000L));
         request.setAgreementDateTo(new Date());
 
