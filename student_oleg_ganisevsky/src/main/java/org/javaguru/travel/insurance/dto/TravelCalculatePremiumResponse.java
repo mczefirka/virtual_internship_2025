f1d8584aa742186e588b9c5275cc03b6d8e8b998
Oleg Ganisevsky <ganisevskij@gmail.com>
2025-04-05 12:53:46 +0300

    STEP 041 - DONE

--- student_oleg_ganisevsky/src/main/java/org/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
+++ student_oleg_ganisevsky/src/main/java/org/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
@@ -1,5 +1,6 @@
 package org.javaguru.travel.insurance.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +17,9 @@ import java.util.List;
 public class TravelCalculatePremiumResponse extends CoreResponse{
     private String personFirstName;
     private String personLastName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date agreementDateFrom;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date agreementDateTo;
     private BigDecimal agreementPrice;
 
