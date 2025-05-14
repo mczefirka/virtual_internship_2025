2de4b73e3c42724f34823f41db9119cddce8d2fb
Oleg Ganisevsky <ganisevskij@gmail.com>
2025-05-14 17:24:12 +0300

    STEP 053 - DONE

--- student_oleg_ganisevsky/src/test/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ student_oleg_ganisevsky/src/test/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -18,103 +19,75 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class TravelCalculatePremiumControllerTest {
 
-    @Autowired private MockMvc mockMvc;
-    @Autowired private JsonFileReader fileReader;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private JsonFileReader fileReader;
 
     @Test
     public void givenRequest_whenRestControllerIsCalled_thenDoNotExpectErrors() throws Exception {
-        String expectedJson = fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumResponse_success.json");
-
         // Compare controller response with expected response
-        mockMvc.perform(post("/insurance/travel/")
-                        .content(fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumRequest_success.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(content().json(expectedJson))
-                .andReturn();
+        compareResponseAndExpectedResponse(
+                "src/test/resources/rest/TravelCalculatePremiumRequest_success.json",
+                "src/test/resources/rest/TravelCalculatePremiumResponse_success.json");
     }
 
     @Test
     public void givenRequestWithoutFirstName_whenRestControllerIsCalled_thenExpectErrors() throws Exception {
-        String expectedJson = fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumResponse_firstName_not_provided.json");
-
-        mockMvc.perform(post("/insurance/travel/")
-                        .content(fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumRequest_firstName_not_provided.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(content().json(expectedJson))
-                .andReturn();
+        compareResponseAndExpectedResponse(
+                "src/test/resources/rest/TravelCalculatePremiumRequest_firstName_not_provided.json",
+                "src/test/resources/rest/TravelCalculatePremiumResponse_firstName_not_provided.json");
     }
 
     @Test
     public void givenRequestWithoutLastName_whenRestControllerIsCalled_thenExpectErrors() throws Exception {
-        String expectedJson = fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumResponse_lastName_not_provided.json");
-
-        mockMvc.perform(post("/insurance/travel/")
-                        .content(fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumRequest_lastName_not_provided.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(content().json(expectedJson))
-                .andReturn();
+        compareResponseAndExpectedResponse(
+                "src/test/resources/rest/TravelCalculatePremiumRequest_lastName_not_provided.json",
+                "src/test/resources/rest/TravelCalculatePremiumResponse_lastName_not_provided.json");
     }
 
     @Test
     public void givenRequestWithoutDateFrom_whenRestControllerIsCalled_thenExpectErrors() throws Exception {
-        String expectedJson = fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumResponse_agreementDateFrom_not_provided.json");
-
-        mockMvc.perform(post("/insurance/travel/")
-                        .content(fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumRequest_agreementDateFrom_not_provided.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(content().json(expectedJson))
-                .andReturn();
+        compareResponseAndExpectedResponse(
+                "src/test/resources/rest/TravelCalculatePremiumRequest_agreementDateFrom_not_provided.json",
+                "src/test/resources/rest/TravelCalculatePremiumResponse_agreementDateFrom_not_provided.json");
     }
 
     @Test
     public void givenRequestWithoutDateTo_whenRestControllerIsCalled_thenExpectErrors() throws Exception {
-        String expectedJson = fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumResponse_agreementDateTo_not_provided.json");
-
-        mockMvc.perform(post("/insurance/travel/")
-                        .content(fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumRequest_agreementDateTo_not_provided.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(content().json(expectedJson))
-                .andReturn();
+        compareResponseAndExpectedResponse(
+                "src/test/resources/rest/TravelCalculatePremiumRequest_agreementDateTo_not_provided.json",
+                "src/test/resources/rest/TravelCalculatePremiumResponse_agreementDateTo_not_provided.json");
     }
 
     @Test
     public void givenRequestWithoutFields_whenRestControllerIsCalled_thenExpectErrors() throws Exception {
-        String expectedJson = fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumResponse_allFields_not_provided.json");
-
-        mockMvc.perform(post("/insurance/travel/")
-                        .content(fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumRequest_allFields_not_provided.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(content().json(expectedJson))
-                .andReturn();
+        compareResponseAndExpectedResponse(
+                "src/test/resources/rest/TravelCalculatePremiumRequest_allFields_not_provided.json",
+                "src/test/resources/rest/TravelCalculatePremiumResponse_allFields_not_provided.json");
     }
 
     @Test
     public void givenRequestWithDateFromIsAfterDateTo_whenRestControllerIsCalled_thenExpectErrors() throws Exception {
-        String expectedJson = fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumResponse_dateFrom_is_after_dateTo.json");
-
-        mockMvc.perform(post("/insurance/travel/")
-                        .content(fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumRequest_dateFrom_is_after_dateTo.json"))
-                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(content().json(expectedJson))
-                .andReturn();
+        compareResponseAndExpectedResponse(
+                "src/test/resources/rest/TravelCalculatePremiumRequest_dateFrom_is_after_dateTo.json",
+                "src/test/resources/rest/TravelCalculatePremiumResponse_dateFrom_is_after_dateTo.json");
     }
 
     @Test
     public void givenRequestWithDateFromIsEqualDateTo_whenRestControllerIsCalled_thenExpectErrors() throws Exception {
-        String expectedJson = fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumResponse_dateFrom_is_equal_dateTo.json");
 
+        compareResponseAndExpectedResponse(
+                "src/test/resources/rest/TravelCalculatePremiumRequest_dateFrom_is_equal_dateTo.json",
+                "src/test/resources/rest/TravelCalculatePremiumResponse_dateFrom_is_equal_dateTo.json");
+    }
+
+    void compareResponseAndExpectedResponse(String requestPath, String expectedResponsePath) throws Exception {
         mockMvc.perform(post("/insurance/travel/")
-                        .content(fileReader.readJsonFromFile("src/test/resources/rest/TravelCalculatePremiumRequest_dateFrom_is_equal_dateTo.json"))
+                        .content(fileReader.readJsonFromFile(requestPath))
                         .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
-                .andExpect(content().json(expectedJson))
+                .andExpect(content().json(fileReader.readJsonFromFile(expectedResponsePath)))
                 .andReturn();
     }
 
